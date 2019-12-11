db2078ac06f95e71f5ed72afa374d741a054ea95
Niels Van Vliet <niels@Nielss-MacBook-Pro.local>
2019-12-11 11:25:59 +0100

    Initial import

---
+++ brew.sh
@@ -0,0 +1,16 @@
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew cask install iterm2
+brew install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/powerline/fonts.git --depth=1
+cd fonts
+./install.sh
+cd ..
+rm -rf fonts
+brew cask install slack
+brew cask install spotify
+brew cask install rectangle
+brew install node
+brew cask install intellij-idea
+brew cask install whatsapp
+brew cask install skype-for-business
